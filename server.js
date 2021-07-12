0b29fbb4b0be1e8d97d11d6eafb513332642b0c6
khalil.zouari <khalilzouari7898@gmail.com>
2021-07-12 23:17:24 +0100

    update to vps

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "http://gaspych.web2code.com:7001",
 		methods: [ "GET", "POST" ]
 	}
 })
@@ -25,4 +25,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(7000, () => console.log("server is running on port 7000"))
